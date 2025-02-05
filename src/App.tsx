1533cf927b4c41aeca59e0f36fb0392ba11ad2d1
Paloma <paloma.correa@dnkinfotelecom.com.br>
2025-02-04 21:59:16 -0300

    feat: include speech recognition and transform the application responsive with sizes format (md:.., sm:...)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import logo from "./assets/logo.png";
 import { NoteCard } from "./components/note-card";
 import { AddCard } from "./components/add-card";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 interface Note {
   id: string,
@@ -10,8 +10,12 @@ interface Note {
 }
 
 export function App() {
- const [notes, setNotes] = useState<Note[]>(() => {
+  const [search, setSearch] = useState('')
+  
+  const [notes, setNotes] = useState<Note[]>(() => {
+  
   const notesOnStorage = localStorage.getItem('notes')
+  
   return notesOnStorage ? JSON.parse(notesOnStorage) : []
  })
 
@@ -30,11 +34,19 @@ export function App() {
 
  }
 
+ function onHandleSearch(event: ChangeEvent<HTMLInputElement>){
+  const query = event.target.value;
+
+  setSearch(query)
+ }
+
+ const filteredNotes = search !== '' ? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())) : notes
+
   return (
     //mx - margin do eixo x - horizontal
     //o tailwind utiliza o espaçamento multiplo de 4, então o valor 1 tem o px de 4, 2 tem px de 8 e vice versa
     //space-y-6 adiciona um espaçamento em cada elemento verticalmente dentro da div
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5 md:px-0">
       <img src={logo} alt="logo" />
       <form className="w-full">
         <input
@@ -42,17 +54,18 @@ export function App() {
           placeholder="Busque em suas notas..."
           //w-full ocupa a largura toda, bg-transparent não tem fundo, text-3xl e tracking-tight deixa menos espaço entre as letras
           className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder: text-slate-500"
+          onChange={onHandleSearch}
         />
       </form>
       {/* incluindo um separador h (horizontal) px (1 px)*/}
       <div className="h-px bg-slate-700" />
 
       {/* para colocarmos um valor definido por mim e não pelo tailwind, posso colocar o valor dentro de colchetes [30px] */}
-      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
         {/* rounded-md é equivalente a border radius */}
         <AddCard onNoteCreated={onNoteCreated}/>
         {/* <NoteCard note={note}/> */}
-        {notes.map(note => {
+        {filteredNotes.map(note => {
           return <NoteCard key={note.id} note={note}/>
         })}
       </div>
